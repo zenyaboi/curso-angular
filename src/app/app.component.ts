a2f17d90f507666201cd4baad4d24f0a1fcc6ab0
zenyaboi <bannedboyinlife@gmail.com>
2022-03-29 16:20:53 -0300

    created new component and passed data created in app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  userName = 'Joaquim';
+
+  userData = {
+    email: 'joaquim@email.com',
+    role: 'Admin'
+  }
+
   title = 'curso-angular';
 }
