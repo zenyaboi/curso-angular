713c56dee206eb506b90d1cad1af4235dd50b633
zenyaboi <bannedboyinlife@gmail.com>
2022-04-11 15:19:42 -0300

    created a component for pipe operator

---
+++ src/app/components/pipes/pipes.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pipes',
+  templateUrl: './pipes.component.html',
+  styleUrls: ['./pipes.component.css'],
+})
+export class PipesComponent implements OnInit {
+  someText = 'Testando o Pipe Operator!';
+
+  today = new Date();
+
+  constructor() {}
+
+  ngOnInit(): void {}
+}
