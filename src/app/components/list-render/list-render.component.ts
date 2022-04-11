e16cc443cd217d04f7611ec118f51c309da153d9
zenyaboi <bannedboyinlife@gmail.com>
2022-04-11 15:11:53 -0300

    created interface Animal

--- src/app/components/list-render/list-render.component.ts
+++ src/app/components/list-render/list-render.component.ts
@@ -1,19 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Animal } from 'src/app/Animal';
+
 @Component({
   selector: 'app-list-render',
   templateUrl: './list-render.component.html',
   styleUrls: ['./list-render.component.css'],
 })
 export class ListRenderComponent implements OnInit {
-  animals = [
-    { name: 'Turca', type: 'Dog' },
-    { name: 'Tom', type: 'Cat' },
-    { name: 'Frida', type: 'Dog' },
-    { name: 'Bob', type: 'Horse' },
+  animals: Animal[] = [
+    { name: 'Turca', type: 'Dog', age: 4 },
+    { name: 'Tom', type: 'Cat', age: 10 },
+    { name: 'Frida', type: 'Dog', age: 5 },
+    { name: 'Bob', type: 'Horse', age: 1 },
   ];
+
+  animalDetails = '';
 
   constructor() {}
 
   ngOnInit(): void {}
+
+  showAge(animal: Animal) {
+    if (animal.age === 1) {
+      this.animalDetails = `O pet ${animal.name} tem ${animal.age} ano!`;
+    } else {
+      this.animalDetails = `O pet ${animal.name} tem ${animal.age} anos!`;
+    }
+  }
 }
